4edf07600983daf6dece2ad444e3c1a871194cf0
INDxSnipzY <snipzy.offical@gmail.com>
2026-09-20 12:51:59 +0530

    Revised Diseass.tsx

--- src/types/disease.ts
+++ src/types/disease.ts
@@ -3,9 +3,6 @@ export interface Disease {
   slug: string;
   name: string;
   image: string;
-  updatedAt: string;
-  readTime: string;
-  medicallyReviewed: boolean;
   category: string;
   overview: string;
   symptoms: string[];
